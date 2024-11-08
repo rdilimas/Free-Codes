82047f5a07aa2ca06a21a98d40a9490f7f2a25a9
rdilimas <rdilimas@gmail.com>
2024-11-07 23:21:39 -0300

    feat: add solution "Hello World" in COBOL

---
+++ 0 - Hello World!/SOLUTION.cbl
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author: Robson Lima
+      * Date: 07/11/2024
+      * Purpose: DEMO
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SOLUTION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM SOLUTION.
